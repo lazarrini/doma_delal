df9406f797bba645fee7eb51b3ee6d1775905e97
Лазарь <139632502+lazarrini@users.noreply.github.com>
2023-07-22 18:07:28 +0300

    Update main.py

--- domashka)/main.py
+++ domashka)/main.py
@@ -2,25 +2,25 @@ import random
 from unit import *
 from weapon import *
 
-
-stas = Knight("Stas")
-vlad = Ranger("Vlad")
-roma = Barbarian("Roma")
-
-axe = Weapon("axe", 6)
-big_sword = Weapon("big_sword", 8)
-
-excalibur = Excalibur()
-
-print(stas)
-print(big_sword)
-print(excalibur)
-Elf.characteristic()
-
-stas.battle_cry()
-stas.attack(vlad)
-
-roma.battle_cry()
-roma.attack(stas, axe)
-stas.attack(roma)
-stas.rest()
+if __name__ == "__main__":
+  stas = Knight("Stas")
+  vlad = Ranger("Vlad")
+  roma = Barbarian("Roma")
+  
+  axe = Weapon("axe", 6)
+  big_sword = Weapon("big_sword", 8)
+  
+  excalibur = Excalibur()
+  
+  print(stas)
+  print(big_sword)
+  print(excalibur)
+  Elf.characteristic()
+  
+  stas.battle_cry()
+  stas.attack(vlad)
+  
+  roma.battle_cry()
+  roma.attack(stas, axe)
+  stas.attack(roma)
+  stas.rest()
